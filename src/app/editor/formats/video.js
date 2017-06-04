022dd9b12e98c132f598dd020fdba1bceb89c5c3
Igor Preston <igorpreston@gmail.com>
2017-06-04 22:16:15 +0300

    format video editing

---
+++ src/app/editor/formats/video.js
@@ -0,0 +1,52 @@
+import Quill from '../setup';
+import { sanitize } from './link';
+
+const BlockEmbed = Quill.import('blots/block/embed');
+
+const ATTRIBUTES = [
+  'height',
+  'width'
+];
+
+export class Video extends BlockEmbed {
+  static create(value) {
+    console.log('initializing', value);
+    let node = super.create(value);
+    node.setAttribute('frameborder', '0');
+    node.setAttribute('allowfullscreen', true);
+    node.setAttribute('src', this.sanitize(value));
+    return node;
+  }
+
+  static formats(domNode) {
+    return ATTRIBUTES.reduce(function(formats, attribute) {
+      if (domNode.hasAttribute(attribute)) {
+        formats[attribute] = domNode.getAttribute(attribute);
+      }
+      return formats;
+    }, {});
+  }
+
+  static sanitize(url) {
+    return sanitize(url, ['http', 'https', 'data']) ? url : '//:0';
+  }
+
+  static value(domNode) {
+    return domNode.getAttribute('src');
+  }
+
+  format(name, value) {
+    if (ATTRIBUTES.indexOf(name) > -1) {
+      if (value) {
+        this.domNode.setAttribute(name, value);
+      } else {
+        this.domNode.removeAttribute(name);
+      }
+    } else {
+      super.format(name, value);
+    }
+  }
+};
+
+Video.blotName = 'video';
+Video.tagName = 'iframe';
